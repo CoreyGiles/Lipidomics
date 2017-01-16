054b927ad1d9005f69f63550a2b2c4898b11733c
Corey Giles <corey.giles@curtin.edu.au>
2017-01-16 14:18:00 +0800

    Update Lipidomics.R

--- Lipidomics.R
+++ Lipidomics.R
@@ -1,3 +1,8 @@
 ####  Lipidomics R file
 
+library(R6)
+
+##  Write code using object oriented programming
+##  Focus on objects (lipids) that get acted on by functions (adducts, ionization etc.)
+
 data<-read.csv(file="data.csv")
