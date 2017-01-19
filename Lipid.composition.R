db663c1b2b64d4baaea0fcf3062c3687f33fb0d1
Corey Giles <corey.giles@curtin.edu.au>
2017-01-19 14:45:13 +0800

    Update Lipid.composition.R

--- Lipid.composition.R
+++ Lipid.composition.R
@@ -1,30 +1,30 @@
-FA<-c("16_0","18_0","18_1","20_0","20_4","20_5","22_0","22_6","24_0","24_1")
-length<-c(16,18,18,20,20,20,22,22,24,24)
-db<-c(0,0,1,0,4,5,0,6,0,1)
+FA<-    c("16_0","16_1","18_0","18_1","18_2","18_3","20_0","20_4","20_5","22_0","22_1","22_5","22_6","24_0","24_1","26_0","26_1")
+length<-c(16,     16,    18,    18,    18,    18,    20,    20,    20,    22,    22,    22,    22,    24,    24,    26,    26)
+db<-    c(0,      1,     0,     1,     2,     3,     0,     4,     5,     0,     1,     5,     6,     0,     1,     0,     1)
 fattyAcids<-data.frame(FA=FA,length=length,db=db)
 
-##  Regression based on real data
-lipids<-read.csv("lipids.csv")
-temp<-lipids
-lipids1<-as.matrix(lipids[,2:24])
-lipids2<-as.matrix(lipids[,26:31])
-linear<-lm(lipids2[,4]~lipids1)
-summary(linear)
+headGroup<-c("PC","PE","PS","PI","OH","TG","Pi","Glu","Lac","Sulfatide")
+
+##  Regression based on lipid data
+#lipids<-read.csv("lipids.csv")
+#temp<-lipids
+#lipids1<-as.matrix(lipids[,2:24])
+#lipids2<-as.matrix(lipids[,26:31])
+#linear<-lm(lipids2[,4]~lipids1)
+#summary(linear)
+#lipidsOffset<-cbind(lipids1,rep(1,nrow(lipids1)))
+#lipids2[,1:6]-lipidsOffset%*%elementalCoef
 ###
 
 ##  Coefficients
-C<-c(3,27,0,5,2,3,6,0,0,6,12,6,1,0,1,0,1,0,0,0,0,0,0,0)
-H<-c(3,41,0,12,6,6,11,0,1,10,20,10,2,-2,2,-2,2,-2,0,2,-2,-2,-2,5)
-N<-c(-1,-1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1)
-O<-c(2,0,0,3,3,5,8,0,3,5,10,8,0,0,0,0,0,0,-1,-1,1,1,1,1)
-P<-c(0,0,0,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-S<-c(0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0)
+#   Gly Chol Sph PC  PE  PS  PI  OH  Pi Glu Lac Sul SN1  DB SN2  DB SN3  DB (P) (O) sn1 sn2 sn3 offset
+C<-c( 3, 27,  0,  5,  2,  3,  6,  0,  0,  6, 12,  6,  1,  0,  1,  0,  1,  0,  0,  0,  0,  0,  0,  0)
+H<-c( 3, 41,  0, 12,  6,  6, 11,  0,  1, 10, 20, 10,  2, -2,  2, -2,  2, -2,  0,  2, -2, -2, -2,  5)
+N<-c(-1, -1,  0,  1,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1)
+O<-c( 2,  0,  0,  3,  3,  5,  8,  0,  3,  5, 10,  8,  0,  0,  0,  0,  0,  0, -1, -1,  1,  1,  1,  1)
+P<-c( 0,  0,  0,  1,  1,  1,  1,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0)
+S<-c( 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0)
 elementalCoef<-matrix(c(C,H,N,O,P,S),ncol=6,dimnames = list(c("Glycerol","Cholesterol","Sphingoid","PC","PE","PS","PI","OH","Pi","Glu","Lac","Sulfatide","SN1-chain","SN1-DB","SN2-chain","SN2-DB","SN3-Chain","SN3-DB","(P)","(O)","sn1","sn2","sn3","offset"),NULL))
-lipidsOffset<-cbind(lipids1,rep(1,nrow(lipids1)))
-lipids2[,1:6]-lipidsOffset%*%elementalCoef
-
-
-c("Glycerol","Cholesterol","Sphingoid","PC","PE","PS","PI","OH","Pi","Glu","Lac","Sulfatide","Chain","DB","Sph-Chain","Sph-DB","(P)","(O)","sn1","sn2","sn3","offset")
 
 generateLipid<-function(backBone="Glycerol",headGroup="PC",acylSN1="18_0",acylSN2="18_1",acylSN3=NA,modification=NA) {
   composition<-numeric(24)
@@ -113,4 +113,32 @@ generateLipid<-function(backBone="Glycerol",headGroup="PC",acylSN1="18_0",acylSN
 
 generateLipid("Sphingoid",headGroup="OH",acylSN1="18_1",acylSN2 = NA)
 
-generateLipid("Sphingoid",headGroup = "OH",acylSN1 = "18_1",acylSN2 = "18_0")%*%elementalCoef
+generateLipid("Sphingoid",headGroup ="OH",acylSN1="18_1",acylSN2="18_0")%*%elementalCoef
+
+
+lipidSet<-matrix(0,nrow=4,ncol=nrow(fattyAcids),dimnames=list(c("Glycerol","SphingoidBase","SphingoidFA","Cholesterol"),fattyAcids$FA))
+lipidSet[c(1,4),]<-1
+lipidSet[2,]<-fattyAcids$FA%in%c("18_0","18_1")
+lipidSet[3,]<-fattyAcids$FA%in%c("16_0","18_0","18_1","20_0","22_0","22_1","24_0","24_1","26_0","26_1")
+
+headGroupSet<-matrix(0,nrow=3,ncol=length(headGroup),dimnames = list(c("Glycerol","Sphingoid","Cholesterol"),headGroup))
+headGroupSet[1,c(1,2,3,4,5,6,7)]<-1
+headGroupSet[2,c(1,5,7,8,9,10)]<-1
+headGroupSet[3,5]<-1
+
+lysoGroupSet<-matrix(0,nrow=3,ncol=length(headGroup),dimnames = list(c("Glycerol","Sphingoid","Cholesterol"),headGroup))
+lysoGroupSet[1,c(1,2,4)]<-1
+lysoGroupSet[2,c(5,7)]<-1
+
+vinylEtherGroupSet<-matrix(0,nrow=3,ncol=length(headGroup),dimnames = list(c("Glycerol","Sphingoid","Cholesterol"),headGroup))
+vinylEtherGroupSet[1,c(1,2)]<-1
+
+etherGroupSet<-matrix(0,nrow=3,ncol=length(headGroup),dimnames = list(c("Glycerol","Sphingoid","Cholesterol"),headGroup))
+etherGroupSet[1,c(1,2)]<-1
+
+lipidMatrix<-list()
+lipidMatrix$lipidSet<-lipidSet
+lipidMatrix$headGroupSet<-headGroupSet
+lipidMatrix$lysoGroupSet<-lysoGroupSet
+lipidMatrix$vinylEtherGroupSet<-vinylEtherGroupSet
+lipidMatrix$etherGroupSet<-etherGroupSet
